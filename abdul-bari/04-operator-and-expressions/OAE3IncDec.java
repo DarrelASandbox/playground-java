0d99f07f55e6fc00456c5f91f986b5187f341b45
Darrel Ang <darrelaiscoding@gmail.com>
2022-11-10 15:48:02 +0800

    04-operator-and-expressions: IncDec

---
+++ abdul-bari/04-operator-and-expressions/OAE3IncDec.java
@@ -0,0 +1,20 @@
+public class OAE3IncDec {
+  public static void main(String[] args) {
+
+    // float x=3.5f
+    // char x='A'
+    // byte x=5
+
+    int x = 5;
+    x++; // ++x
+    System.out.println("5++:  " + x);
+
+    int x1 = 5, y1;
+    y1 = x1++; // ++x1
+    System.out.println("5: " + x1 + "   5++: " + y1);
+
+    int x2 = 5, y2 = 4, z2;
+    z2 = 2 * x2++ + 3 * ++y2;
+    System.out.println("2 * 5++ + 3 * ++4: " + z2);
+  }
+}
