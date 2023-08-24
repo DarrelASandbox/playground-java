9e69c6329e18624bd2033e88a2a60281c9a35122
DarrelA <darrelaiscoding@gmail.com>
2023-08-24 23:19:34 +0800

    Display User ID and Roles & restrict URLs based on roles

--- chad-darby/_12_spring_security_demo/src/main/java/com/darrelasandbox/_12_spring_security_demo/controller/DemoController.java
+++ chad-darby/_12_spring_security_demo/src/main/java/com/darrelasandbox/_12_spring_security_demo/controller/DemoController.java
@@ -8,7 +8,19 @@ public class DemoController {
 
     @GetMapping("/")
     public String showHome() {
-
         return "home";
     }
+
+    // add a request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
+
+    // add request mapping for /systems
+    @GetMapping("/systems")
+    public String showSystems() {
+        return "systems";
+    }
+
 }
