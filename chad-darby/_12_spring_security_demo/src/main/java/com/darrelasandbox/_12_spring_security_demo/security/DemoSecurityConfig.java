509ea412c757b85faa1d3b49568d8feac1c08113
DarrelA <darrelaiscoding@gmail.com>
2023-08-25 12:41:04 +0800

    JDBC authentication & BCrypt

--- chad-darby/_12_spring_security_demo/src/main/java/com/darrelasandbox/_12_spring_security_demo/security/DemoSecurityConfig.java
+++ chad-darby/_12_spring_security_demo/src/main/java/com/darrelasandbox/_12_spring_security_demo/security/DemoSecurityConfig.java
@@ -1,38 +1,24 @@
 package com.darrelasandbox._12_spring_security_demo.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+  // Inject data source
+  // Auto-configured by Spring Boot
   @Bean
-  public InMemoryUserDetailsManager userDetailsManager() {
-
-    UserDetails john = User.builder()
-        .username("john")
-        .password("{noop}p")
-        .roles("EMPLOYEE")
-        .build();
-
-    UserDetails mary = User.builder()
-        .username("mary")
-        .password("{noop}p")
-        .roles("EMPLOYEE", "MANAGER")
-        .build();
-
-    UserDetails susan = User.builder()
-        .username("susan")
-        .password("{noop}p")
-        .roles("EMPLOYEE", "MANAGER", "ADMIN")
-        .build();
-
-    return new InMemoryUserDetailsManager(john, mary, susan);
+  public UserDetailsManager userDetailsManager(DataSource dataSource) {
+      // Tell Spring Security to use JDBC authentication with our data source
+      return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
