d7bac9ad5a58caf8fa50c7494042601fd6c01595
DarrelA <darrelaiscoding@gmail.com>
2023-09-26 08:04:09 +0800

    `_11InterProcess`: `synchronized`

--- abdul-bari/src/_19multithreading/_11InterProcess.java
+++ abdul-bari/src/_19multithreading/_11InterProcess.java
@@ -2,16 +2,36 @@ package src._19multithreading;
 
 class MyData2 {
   int value;
+  boolean flag = true;
 
-  public void set(int v) {
+  synchronized public void set(int v) {
+    while (flag != true) {
+      try {
+        wait();
+      } catch (Exception e) {
+      }
+    }
+
     value = v;
+    flag = false;
+    notify();
   }
 
-  public int get() {
+  synchronized public int get() {
     int x = 0;
+    while (flag != false) {
+      try {
+        wait();
+      } catch (Exception e) {
+      }
+    }
+
     x = value;
+    flag = true;
+    notify();
     return x;
   }
+
 }
 
 class Producer extends Thread {
